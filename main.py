a88be054180fd67f1abd637f8b9ce3a966b759be
mustafakemal1729 <experimus1@gmail.com>
2024-01-04 18:29:45 +0100

    refactor

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+""" import libraries """
 import tkinter as tk
 from ui import TextEditorUI
 
+
 def main():
+    """Main program entry point."""
     window = tk.Tk()
     text_editor_ui = TextEditorUI(window)
     window.mainloop()
+
 
 if __name__ == "__main__":
     main()
